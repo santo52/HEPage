f6240eaad471baeca13ecae8aed7f692036c2f63
Santiago Ruiz <sanruiz1003@gmail.com>
2016-03-18 18:35:14 -0500

    Implementacion de menus sin responsive

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,64 @@ $('.company_acordeon_item').on('click', function(){
     $(this).toggleClass('visible');
 });
 
-var $menuButtons = $('.header_buttons');
+var $closeTab = $('.tab_close');
+var $headerButtons = $('.header_buttons');
 var $header = $('.header');
 var $document = $(document);
+var $menuButton = $('.menu_button');
+var $contactButton = $('.contact_button');
 
 $document.on('scroll', function(){
     if($document.scrollTop() > $header.height()){
-        $menuButtons.addClass('fixedMenu');
+        $headerButtons.addClass('fixedMenu');
     }
     else{
-        $menuButtons.removeClass('fixedMenu');
+        $headerButtons.removeClass('fixedMenu');
     }
+});
+
+
+$closeTab.on('click', function(){
+    $(this).parent().removeClass('slideShow');
+});
+
+$menuButton.on('click', function(){
+    $('.menu').addClass('slideShow');
+});
+
+$contactButton.on('click', function(){
+    $('.contact').addClass('slideShow');
+});
+
+var contact = $('.contact');
+var menu = $('.menu');
+
+//***************************//
+//***************************//
+//******** PESTAÑAS *********//
+//***************************//
+//***************************//
+
+$('.contact .pestana').click(function(){
+    contact.css('z-index', '20').animate({right: "0", border: 0}, 1000);
+    $('html').css('overflow', 'hidden');
+});
+
+$('.contact .labels').on('click', 'a', function(){
+    contact.css('z-index', '0').animate({right: "-99.5%"}, 1000).css('border-left', '10px solid #009B93');
+    $('html').css('overflow-y', 'scroll');
+})
+
+$('.menu .pestana').click(function(){
+    menu.css('z-index', '20').animate({left: "0", border: 0}, 1000);
+    $('html').css('overflow', 'hidden');
+});
+
+$('.menu .labels').on('click', 'a', function(){
+    menu.css('z-index', '0').animate({left: "-99.5%"}, 1000).css('border-right', '10px solid #00637A');
+    $('html').css('overflow-y', 'scroll');
+})
+
+$('.agencia__team__members__member').hover(function(){
+    $(this).children('.icon').stop().slideToggle(300);
 });
